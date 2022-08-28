674ed8198eb16f6eca602bb1dc9ce4c001036239
Kendall Kant <kantkendall@gmail.com>
2022-08-27 23:59:52 -0500

    :sparkles: feat(frontend): hotel card with grid

--- frontend/src/components/Hotels.jsx
+++ frontend/src/components/Hotels.jsx
@@ -5,6 +5,9 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
 
 // const bull = (
 //   <Box
@@ -14,6 +17,14 @@ import Typography from "@mui/material/Typography";
 //     •
 //   </Box>
 // );
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const serverURL = import.meta.env.PUBLIC_API_URL;
 
@@ -48,40 +59,48 @@ export default function Hotels() {
   return (
     <>
       <a>Test App</a>
+
+      {/* <a> {`${doc.title}`} </a> */}
       {!isHotelDataLoading ? (
-        hoteslInfoState.map((doc, index) => {
-          return (
-            <div key={index} className="flex items-start">
-              {/* <a> {`${doc.title}`} </a> */}
-              <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                  <Typography
-                    sx={{ fontSize: 14 }}
-                    color="text.secondary"
-                    gutterBottom
-                  >
-                    {`${doc.title}`}
-                  </Typography>
-                  <Typography variant="h6" component="div">
-                    {`${doc.name}`}
-                  </Typography>
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    {`${doc.address}`}
-                  </Typography>
-                  <Typography variant="body2">
-                    {`${doc.url}`}
-                    <br />
-                    {'"Lorem Ipsum"'}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-              <a> -------- </a>
-            </div>
-          );
-        })
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 2, sm: 8, md: 12 }}
+          >
+            {hoteslInfoState.map((doc, index) => (
+              <Grid item xs={2} sm={4} md={4} key={index}>
+                <Item>
+                  <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                      <Typography
+                        sx={{ fontSize: 14 }}
+                        color="text.secondary"
+                        gutterBottom
+                      >
+                        {`${doc.title}`}
+                      </Typography>
+                      <Typography variant="h6" component="div">
+                        {`${doc.name}`}
+                      </Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {`${doc.address}`}
+                      </Typography>
+                      <Typography variant="body2">
+                        {`${doc.url}`}
+                        <br />
+                        {'"Lorem Ipsum"'}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Item>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       ) : (
         <div>
           <a>Loading hotels...</a>
